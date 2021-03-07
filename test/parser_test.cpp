f56872b2ddba82511570d3eb3b2dd56c7c627bf0
Kyle Nahrgang <knahrgang@lifeshield.com>
2021-03-07 06:40:50 -0500

    add a simple cppunit test

--- test/parser_test.cpp
+++ test/parser_test.cpp
@@ -1,1 +1,20 @@
 #include <cpp8601.hpp>
+#include <cppunit/extensions/HelperMacros.h>
+
+class cpp8601_parser_test_fixture : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(cpp8601_parser_test_fixture);
+    CPPUNIT_TEST(testTrue);
+    CPPUNIT_TEST_SUITE_END();
+
+  public:
+    void setUp();
+    void tearDown();
+
+    void testTrue();
+};
+
+void cpp8601_parser_test_fixture::setUp() {}
+void cpp8601_parser_test_fixture::tearDown() {}
+void cpp8601_parser_test_fixture::testTrue() {
+    CPPUNIT_ASSERT(true);
+}
